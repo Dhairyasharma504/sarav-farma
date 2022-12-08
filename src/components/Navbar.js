f7444a9499c13dd1d45802ed2f4b6144bfd7c0bf
dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-12-08 13:09:16 +0530

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
 
-            <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
+            <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboarddd</a>
 
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
 
